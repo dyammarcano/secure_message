7a6eac3756554353524d74b964eae71efb2f9ec2
Dyam Antonio Marcano Daniels <dyam.daniels@b3.com.br>
2023-11-09 23:39:58 -0300

    changes

--- internal/algorithm/cryptography/cryptography_test.go
+++ internal/algorithm/cryptography/cryptography_test.go
@@ -1,29 +1,28 @@
 package cryptography
 
 import (
-	"github.com/dyammarcano/secure_message/internal/mock"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestEncryptString(t *testing.T) {
-	encrypted, err := AutoEncryptString(mock.Message1kChars)
+	encrypted, err := AutoEncryptString("hello world")
 	assert.Nil(t, err)
 
 	decrypted, err := AutoDecryptString(encrypted)
 	assert.Nil(t, err)
 
-	assert.Equal(t, decrypted, mock.Message1kChars)
+	assert.Equal(t, decrypted, "hello world")
 }
 
 func TestEncryptBytes(t *testing.T) {
-	encrypted, err := AutoEncryptBytes([]byte(mock.Message1kChars))
+	encrypted, err := AutoEncryptBytes([]byte("hello world"))
 	assert.Nil(t, err)
 
 	decrypted, err := AutoDecryptBytes(encrypted)
 	assert.Nil(t, err)
 
-	assert.Equal(t, decrypted, []byte(mock.Message1kChars))
+	assert.Equal(t, decrypted, []byte("hello world"))
 }
 
 func TestEncrypt(t *testing.T) {
--- internal/algorithm/encoding/encoding_test.go
+++ internal/algorithm/encoding/encoding_test.go
@@ -1,57 +1,16 @@
 package encoding
 
 import (
-	"github.com/dyammarcano/secure_message/internal/mock"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestEncoding1kChars(t *testing.T) {
-	serialized, err := Serialize(mock.Message1kChars)
+	serialized, err := Serialize("hello world")
 	assert.Nil(t, err)
 
 	deserialized, err := Deserialize(serialized)
 	assert.Nil(t, err)
 
-	assert.Equal(t, deserialized, mock.Message1kChars)
-}
-
-func TestEncoding2kChars(t *testing.T) {
-	serialized, err := Serialize(mock.Message2kChars)
-	assert.Nil(t, err)
-
-	deserialized, err := Deserialize(serialized)
-	assert.Nil(t, err)
-
-	assert.Equal(t, deserialized, mock.Message2kChars)
-}
-
-func TestEncoding3kChars(t *testing.T) {
-	serialized, err := Serialize(mock.Message3kChars)
-	assert.Nil(t, err)
-
-	deserialized, err := Deserialize(serialized)
-	assert.Nil(t, err)
-
-	assert.Equal(t, deserialized, mock.Message3kChars)
-}
-
-func TestEncoding4kChars(t *testing.T) {
-	serialized, err := Serialize(mock.Message4kChars)
-	assert.Nil(t, err)
-
-	deserialized, err := Deserialize(serialized)
-	assert.Nil(t, err)
-
-	assert.Equal(t, deserialized, mock.Message4kChars)
-}
-
-func TestEncoding5kChars(t *testing.T) {
-	serialized, err := Serialize(mock.Message5kChars)
-	assert.Nil(t, err)
-
-	deserialized, err := Deserialize(serialized)
-	assert.Nil(t, err)
-
-	assert.Equal(t, deserialized, mock.Message5kChars)
+	assert.Equal(t, deserialized, "hello world")
 }
